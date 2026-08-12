629387b220ff32cd5c19e76fa556fa4f9732a21f
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-08-12 22:41:04 +0800

    Add API-prefixed leave type routes

--- backend/src/main/java/com/practical/leavemaster/leavetype/LeaveTypeController.java
+++ backend/src/main/java/com/practical/leavemaster/leavetype/LeaveTypeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/leave-types")
+@RequestMapping({"/leave-types", "/api/leave-types"})
 @RequiredArgsConstructor
 public class LeaveTypeController {
 
