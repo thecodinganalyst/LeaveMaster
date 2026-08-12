679a05402ab4ba30dfdc8322b0e1d57c95afddb4
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-08-12 22:40:29 +0800

    Add API-prefixed role routes

--- backend/src/main/java/com/practical/leavemaster/rbac/AppRoleController.java
+++ backend/src/main/java/com/practical/leavemaster/rbac/AppRoleController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/roles")
+@RequestMapping({"/roles", "/api/roles"})
 @RequiredArgsConstructor
 public class AppRoleController {
 
