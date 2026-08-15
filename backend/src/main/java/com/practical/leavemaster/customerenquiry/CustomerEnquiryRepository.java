a5bb9a407291268477763ae2f6904850f2b98fb9
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-08-15 14:20:33 +0800

    Add customer enquiry repository

---
+++ backend/src/main/java/com/practical/leavemaster/customerenquiry/CustomerEnquiryRepository.java
@@ -0,0 +1,6 @@
+package com.practical.leavemaster.customerenquiry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerEnquiryRepository extends JpaRepository<CustomerEnquiry, String> {
+}
