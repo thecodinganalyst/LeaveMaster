e9d36fca48bfeb4c7b884a2e869edf5c8c5e1039
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-08-15 14:20:43 +0800

    Add customer enquiry validation error

---
+++ backend/src/main/java/com/practical/leavemaster/customerenquiry/CustomerEnquiryValidationException.java
@@ -0,0 +1,7 @@
+package com.practical.leavemaster.customerenquiry;
+
+public class CustomerEnquiryValidationException extends RuntimeException {
+    public CustomerEnquiryValidationException(String message) {
+        super(message);
+    }
+}
