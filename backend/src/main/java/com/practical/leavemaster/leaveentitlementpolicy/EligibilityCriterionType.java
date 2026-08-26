b4ad248282bec6b158a38846114e76cce99fa436
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2026-08-26 22:45:36 +0800

    Add employment type eligibility criterion

--- backend/src/main/java/com/practical/leavemaster/leaveentitlementpolicy/EligibilityCriterionType.java
+++ backend/src/main/java/com/practical/leavemaster/leaveentitlementpolicy/EligibilityCriterionType.java
@@ -3,5 +3,6 @@ package com.practical.leavemaster.leaveentitlementpolicy;
 public enum EligibilityCriterionType {
     JURISDICTION_CODE,
     SERVICE_MONTHS,
+    EMPLOYMENT_TYPE,
     HAS_DEPENDANT_MATCHING
 }
